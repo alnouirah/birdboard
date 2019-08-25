cad5d6f34f7328bd02b168c97110f11e0775145b
ammar <alnouirah@gmail.com>
2019-08-25 20:40:04 +0300

    testing cycle

---
+++ app/projects.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class projects extends Model
+{
+    //
+    protected $guarded = [];
+}
--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,8 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::post('/projects',function(){
+    App\projects::create(request(['title','description']));
+});
