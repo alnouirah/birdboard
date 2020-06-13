d87dfa87a0015cae7d759512819936b1e7a44e13
ammar_alnouirah <alnouirah@gmail.com>
2020-06-13 20:24:51 +0300

    new name of projects

---
+++ app/Project.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Project extends Model
+{
+    
+    protected $guarded = [];
+
+
+    public function path()
+    {
+
+        return "/projects/{$this->id}";
+
+    }
+}
